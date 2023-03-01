7dbbaee2313d5d795da7d96258164f9db91646a0
Aervue <aervue.dev@gmail.com>
2023-03-01 20:44:08 +0200

    change fee / change route

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,16 +1,12 @@
-
 const routes = [
   {
-    path: '/',
+    path: '',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: '/market', component: () => import('pages/MarketPage.vue') }
+      { path: 'market', component: () => import('pages/MarketPage.vue') }
     ]
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
