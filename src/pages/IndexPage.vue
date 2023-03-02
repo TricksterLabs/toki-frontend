835443be08ae289cb19eb048caf7ceaccb9483f5
Aervue <aervue.dev@gmail.com>
2023-03-02 19:28:00 +0200

    change price

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -597,7 +597,7 @@ export default defineComponent({
         return 0
       }
       // parseInt((this.session_data.order_vbytes_count * (this.market_data.selected_fee!='Custom'?gas_fee_data[this.market_data.selected_fee]:this.market_data.custom_fee)) * 1.25 + this.session_data.order_file_count * 25546
-      return parseInt((this.session_data.order_vbytes_count * fee) * 1.25 + this.session_data.order_file_count * 25546);
+      return parseInt((this.session_data.order_vbytes_count * fee) * 1 + this.session_data.order_file_count * 546 + 2500);
     }
   }
 })
